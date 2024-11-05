a2bed798397945d67c07e6adfbccd2476246a6ee
HUNGVP <hungvp1104@gmail.com>
2024-11-05 10:47:52 +0700

    feat: upload folder Testing

---
+++ Impact Labs/Execute test/register-google.spec.ts
@@ -0,0 +1,35 @@
+import { test, expect } from '@playwright/test';
+
+
+test.describe('Register with Google', () => {
+    const email = 'hungvp@hblab.vn';
+    const emailPassword = 'Hung@1995';
+
+    test('Register', async ({ page }) => {
+        await test.step('Go to sign up page', async () => {
+            await page.goto('https://hblstaging.impactlabs.org/');
+            await page.getByRole('link', { name: 'Sign up', exact: true }).click();
+        });
+
+        await test.step('Continue with Google', async () => {
+            await page.getByRole('button', { name: 'Continue with Google', exact: true }).click();
+            await page.locator('//input[@type="email"]').fill(email);
+
+        });
+        await test.step('Submit the email Google', async () => {
+            await page.getByRole('button', { name: 'Next', exact: true }).click();
+        });
+
+        await test.step('Input email password', async () => {
+            await page.locator('//input[@type="password"]').fill(emailPassword);
+        });
+
+        await test.step('Submit the email password', async () => {
+            await page.getByRole('button', { name: 'Next', exact: true }).click();
+        });
+
+        await test.step('Approve Sign in with Google', async () => {
+            await page.getByRole('button', { name: 'Continue', exact: true }).click();
+        });
+    });
+})
---
+++ Impact Labs/Execute test/register-manual.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test('Register', async ({ page }) => {
+    await test.step('Go to sign up page', async () => {
+        await page.goto('https://hblstaging.impactlabs.org/');
+        await page.getByRole('link', { name: 'Sign up', exact: true }).click();
+    });
+
+    // Sign up manually
+    await test.step('Fill out the form', async () => {
+        await page.locator('//input[@placeholder="miles@spidey.web"]').fill('hungvp@hblab.vn');
+    });
+
+    await test.step('Submit the form', async () => {
+        await page.getByRole('button', { name: 'Continue' , exact: true}).click();
+    });
+});
+
